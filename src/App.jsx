c01c6f291dabf90b50833fa31392307ca853407d
tshimsthedoor <tshims79@gmail.com>
2023-10-24 16:00:03 +0200

    My first commit on vite and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <button className="btn">Button</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
